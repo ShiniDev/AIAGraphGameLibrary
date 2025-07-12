6507a631840b60bc28fb4fe134433ba14b54c584
ShiniDev <markjoefreylaurente@gmail.com>
2025-07-12 16:30:04 -0400

    feat: Moved root karts/slimes as an example file

--- slime-example.php
+++ slime-example.php
@@ -20,22 +20,11 @@ const STAT_JUMP = 1;
 
 $slimeHelper->initializeSlime("ShiniDev", "Philippines", "Light Grey", STAT_SPEED, STAT_ACCELERATION, STAT_JUMP);
 $ballPosition = $slimeHelper->createSlimeGetVector3(GetSlimeVector3Modifier::BALL_POSITION)->getOutput();
-$selfPosition = $slimeHelper->createSlimeGetVector3(GetSlimeVector3Modifier::SELF_POSITION)->getOutput();
-$selfTransform = $slimeHelper->createVolleyballGetTransform(VolleyballGetTransformModifier::SELF)->getOutput();
-$posBackward = $slimeHelper->createRelativePosition(RelativePositionModifier::BACKWARD);
-$posBackward->connectInput($selfTransform);
-
-$moveToBall = $mathHelper->getScaleVector3($posBackward->getOutput(), .4);
-$moveToBall = $mathHelper->getAddVector3($ballPosition, $moveToBall);
 
 // Jumping logic
 $distance = $mathHelper->getDistance($ballPosition, $selfPosition);
 $shouldJump = $slimeHelper->compareFloats(FloatOperator::LESS_THAN, $distance, 2.25);
 
-$slimeHelper->controller($moveToBall, $shouldJump);
-
-
-
-
+$slimeHelper->controller($ballPosition, $shouldJump);
 
 $graph->toTxt("C:\Users\Haba\Downloads\SlimeVolleyball_v0_6\AIComp_Data\Saves\shini.txt");
